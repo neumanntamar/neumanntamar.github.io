c031b004a49b659e75b2375000bd2b108c637048
Gittie Klein <gittiekay@gmail.com>
2016-10-10 17:09:41 -0400

    Added the class that generates a random word together with 3 enumerated types, one for each level word. And tested out a random word in main.

---
+++ src/HangmanMain.java
@@ -0,0 +1,14 @@
+import words.*;
+import java.util.Scanner;
+
+public class HangmanMain 
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.print("Please enter the level you would like to play: easy, medium or hard: ");
+		String level = input.nextLine();
+		GenerateWord word = new GenerateWord(level);
+		System.out.println(word.getWord());
+	}
+}
